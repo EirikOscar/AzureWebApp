fa8ba428d4dd37f92ebe5e872f17f3e9c0ec9ce8
EirikOscar <eirik.oscar.horgen.ness@gmail.com>
2021-02-15 11:06:02 +0100

    test

--- on-prem/hyper-V/Domain-join/domain-join.ps1
+++
@@ -1,6 +0,0 @@
-
-#Legge maskiner inn i domenet
-foreach($c in Get-NetAdapter) {
-    Set-DnsClientServerAddress -InterfaceIndex $c.interfaceindex -ServerAddresses ('192.168.0.11')
-}
-Add-Computer -DomainName "LEVER.no" -restart
